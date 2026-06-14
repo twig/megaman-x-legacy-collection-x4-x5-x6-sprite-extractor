1cf9df05c1cc0cd9c167245bf92eae713d92c665
Twig N <koonkii@hotmail.com>
2026-06-15 08:20:56 +1000

    compare-baseline - moved x5 files

--- compare-baseline-accept.bat
+++ compare-baseline-accept.bat
@@ -1,5 +1,5 @@
 @REM Rename all "*_level.png" to "*_level-baseline.png" for git diff comparison.
-for %%f in (progress-baseline\*_level.png) do (
+for %%f in (progress-baseline\X5\*_level.png) do (
     del "%%~dpf%%~nf-baseline%%~xf" 2>nul
     ren "%%f" "%%~nf-baseline%%~xf"
 )
